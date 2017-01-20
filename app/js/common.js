6ae69451675d50abc551dd6042a7f81e532dff7f
Ai. Kirill <aist.k.work@gmail.com>
2017-01-20 18:40:00 +0200

    Weather app ready, not fully responsive

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,8 @@
 $(function() {
 
-	// Custom JS
-	// $(document).ready(function(){
-	// 	console.log("Hello");
-	// })
-
 	var x = document.getElementById("geo");
+
+	//If no more requests by ipInfo
 
 	// var geolocation = function() {
 	// 	if (navigator.geolocation) {
@@ -22,7 +19,6 @@ $(function() {
 	// showPosition(geolocation());
 
 		
-	var city;
 
 
 	var getInfoAboutUser = function(){
@@ -30,36 +26,69 @@ $(function() {
 		// console.log(response.city, response.region,  response.country, response.loc);
 		city = ( response.city);
 		console.log(city);
-		x.innerHTML = "City: " + response.city + "<br>Country: " + response.country + "<br>" + response.loc;
+
+				//Weather info
+	var temp = document.getElementById('temp');
+	var weather = document.getElementById('weather');
+	
+	var z = $.get('http://api.openweathermap.org/data/2.5/weather', { appid : '334e12fbfc05f6bcc98770eca8633a9b', q : city }, function(response){
+		console.log(response.cod + ' ' + response.name + ' ' + response.main.temp + ' ' + response.weather[0].main);
+		var weatherMain = response.weather[0].main ;
+		var KelvinT = response.main.temp;
+		var Celcius = (KelvinT - 273).toFixed();
+
+		var Fahrenheit = Celcius * (9/5) + 32;
+		console.log(Celcius + " " + KelvinT + ' Fahrenheit: ' + Fahrenheit);
+
+		
+
+
+		var tempOut = function(units){
+			temp.innerHTML = ' ';
+			temp.innerHTML += " " + units + " ";
+		}
+
+
+		weather.innerHTML += " " + weatherMain;
+
+
+		$('#Fahrenheit').on('click', function(){
+				$(this).addClass('active').css({ 'opacity' : '1'});
+				$('#celsius').removeClass('active').css({'opacity' : '0.5'});
+				$('#temp').css('font-size', '8vw');
+
+				
+				tempOut(Fahrenheit);
+		});
+
+		$('#celsius').on('click', function(){
+			$(this).addClass('active').css('opacity', '1');
+			$('#Fahrenheit').removeClass('active').css('opacity', '0.5');
+
+			tempOut(Celcius);
+		});
+
+		if($("#celsius").hasClass('active')){
+			tempOut(Celcius);
+		} else {
+			tempOut(Fahrenheit);
+		}
+
+		
+	},"jsonp");
+
+		x.innerHTML = " " + response.city + " , " + response.country ;
 	}, "jsonp");
+
 
 
 };
 
+	 getInfoAboutUser();
 
 
-	getInfoAboutUser();
-
-	
 
 
-	// var getTheWeather = function(city){
-	// 	console.log(city);
-		// $.get('http://api.openweathermap.org/data/2.5/weather', { appid : '334e12fbfc05f6bcc98770eca8633a9b', q : city }, function(response){
-		// 	console.log(response.cod + ' ' + response.name);
-	
 
-	// getTheWeather(city);
 
-	// var z = $.get('http://api.openweathermap.org/data/2.5/weather', { appid : '334e12fbfc05f6bcc98770eca8633a9b', q : 'Kiev' }, function(response){
-	// 	console.log(response.cod + ' ' + response.name);
-	// },"jsonp");
-
-	// var url      = window.location.href ;
-
-	// var newurl = 'http://google.com';
-
-	// window.location.href = newurl;
-
-	// console.log(url, typeof y);
 });
